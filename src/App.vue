b3e1ce7896aaa5a97aa324f4c424d59eb54c0394
David Royer <droyer01@gmail.com>
2017-01-15 23:59:04 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- Don't drop "q-app" class -->
+  <div id="q-app">
+<div>
+  <div class="routes">
+    <router-link to="/test">Test</router-link>
+    <router-link to="/test2">Test2</router-link>
+  </div>
+</div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+/*
+ * Root component
+ */
+export default {}
+</script>
+
+<style></style>
---
+++ templates/component.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Testing Generation of components</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+</style>
